e777ffd19e8c6ef95942435fdba799fd1d30b215
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-23 12:12:01 -0300

    Update createCharacterService.js

--- src/modules/character/createCharacter/createCharacterService.js
+++ src/modules/character/createCharacter/createCharacterService.js
@@ -1,10 +1,17 @@
 const prisma = require("../../../database/index");
 
-const createCharacterService = async ({ name, occupation, age, archetype }) => {
+const createCharacterService = async ({
+  name,
+  occupation,
+  age,
+  archetype,
+  player_id,
+}) => {
   const character = await prisma.character.create({
     data: {
       name,
       occupation,
+      player_id,
       age,
       archetype,
     },
