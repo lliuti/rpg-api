defd02fd3a7ee163be52a61771de58dddda5abd0
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-23 12:12:02 -0300

    Update authenticatePlayerController.js

--- src/modules/player/authenticatePlayer/authenticatePlayerController.js
+++ src/modules/player/authenticatePlayer/authenticatePlayerController.js
@@ -7,6 +7,7 @@ const authenticatePlayerController = async (req, res) => {
     const token = await authenticatePlayerService({ username, password });
     return res.status(201).json(token);
   } catch (err) {
+    console.log(err);
     return res.status(400).json({ error: err.message });
   }
 };
