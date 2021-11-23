e12c7857ed945692fe7bc0aaa08eb8d30b785c08
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-23 13:45:33 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ const Router = require("express/lib/router");
 const createPlayer = require("./modules/player/createPlayer/createPlayerController");
 const authenticatePlayer = require("./modules/player/authenticatePlayer/authenticatePlayerController");
 const createCharacter = require("./modules/character/createCharacter/createCharacterController");
+const listMyCharacters = require("./modules/character/listMyCharacters/listMyCharactersController");
+const getCharacterSheet = require("./modules/character/getCharacterSheet/getCharacterSheetController");
 const ensureAuthenticated = require("./middlewares/ensureAuthenticated");
 
 const routes = Router();
@@ -13,8 +15,16 @@ routes.post("/players", createPlayer);
 // AUTHENTICATE PLAYER
 routes.post("/login", authenticatePlayer);
 
+// -----------------
+
 // CHARACTERS
 // CREATE CHARACTER
 routes.post("/characters", ensureAuthenticated, createCharacter);
+
+// LIST MY CHARACTERS
+routes.get("/characters", ensureAuthenticated, listMyCharacters);
+
+// GET CHARACTER SHEET
+routes.get("/characters/:character_id", ensureAuthenticated, getCharacterSheet);
 
 module.exports = routes;
