d8caf26f7d8a9bfcc005e87365cbc81a37395007
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-23 11:22:02 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,20 @@
 const Router = require("express/lib/router");
 const createPlayer = require("./modules/player/createPlayer/createPlayerController");
 const authenticatePlayer = require("./modules/player/authenticatePlayer/authenticatePlayerController");
+const createCharacter = require("./modules/character/createCharacter/createCharacterController");
+const ensureAuthenticated = require("./middlewares/ensureAuthenticated");
 
 const routes = Router();
 
 // PLAYERS
+// CREATE PLAYER
 routes.post("/players", createPlayer);
 
 // AUTHENTICATE PLAYER
 routes.post("/login", authenticatePlayer);
+
+// CHARACTERS
+// CREATE CHARACTER
+routes.post("/characters", ensureAuthenticated, createCharacter);
 
 module.exports = routes;
