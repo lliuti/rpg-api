8828641e161df746c3382d044782818db274dd11
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-23 12:12:04 -0300

    Update createPlayerController.js

--- src/modules/player/createPlayer/createPlayerController.js
+++ src/modules/player/createPlayer/createPlayerController.js
@@ -12,6 +12,7 @@ const createPlayerController = async (req, res) => {
     });
     return res.status(201).json(player);
   } catch (err) {
+    console.log(err);
     return res.status(400).json({ error: err.message });
   }
 };
