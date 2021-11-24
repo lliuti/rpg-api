ca35f6f1d7ef607505655a2793d66fbd43dcfe09
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-24 09:26:56 -0300

    Create isAdminController.js

---
+++ src/modules/player/isAdmin/isAdminController.js
@@ -0,0 +1,15 @@
+const isAdminService = require("./isAdminService");
+
+const isAdminController = async (req, res) => {
+  const { player_id } = req.params;
+
+  try {
+    const isAdmin = await isAdminService(player_id);
+    return res.status(200).json(isAdmin);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ error: err.message });
+  }
+};
+
+module.exports = isAdminController;
