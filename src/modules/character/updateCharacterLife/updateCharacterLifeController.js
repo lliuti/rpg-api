020b63fe19b7d74936fa47c1156f0175794eaaca
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-24 08:11:38 -0300

    Create updateCharacterLifeController.js

---
+++ src/modules/character/updateCharacterLife/updateCharacterLifeController.js
@@ -0,0 +1,19 @@
+const updateCharacterLifeService = require("./updateCharacterLifeService");
+
+const updateCharacterLifeController = async (req, res) => {
+  const { currLife, maxLife } = req.body;
+  const { character_id } = req.params;
+  try {
+    const lifePoints = await updateCharacterLifeService({
+      character_id,
+      currLife,
+      maxLife,
+    });
+    return res.status(201).json(lifePoints);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ error: err.message });
+  }
+};
+
+module.exports = updateCharacterLifeController;
