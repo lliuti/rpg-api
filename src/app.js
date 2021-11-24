0196c04c3b51003c4ed32c1ecab3d8def5eeb55d
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-24 11:22:10 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,21 +7,26 @@ const { Server } = require("socket.io");
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  cors: {
-    origin: "*",
-  },
-});
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-const PORT = process.env.PORT || 3333;
+io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
 
 io.on("connection", (socket) => {
   console.log(socket.id);
 });
+
+exports.getSocketInstance = function () {
+  return io;
+};
+
+const PORT = process.env.PORT || 3333;
 
 httpServer.listen(PORT, () => {
   console.log(`Server running on port: ${PORT} with websockets`);
