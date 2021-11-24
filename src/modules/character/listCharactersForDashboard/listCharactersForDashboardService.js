de706e6567ba363e9622c9b87bc3fa56f21e21f9
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-24 09:26:59 -0300

    Create listCharactersForDashboardService.js

---
+++ src/modules/character/listCharactersForDashboard/listCharactersForDashboardService.js
@@ -0,0 +1,31 @@
+const prisma = require("../../../database/index");
+
+const listCharactersForDashboardService = async () => {
+  const list = await prisma.character.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+    select: {
+      id: true,
+      name: true,
+      archetype: true,
+      picture_url: true,
+      exp: true,
+      sheet: {
+        select: {
+          curr_life: true,
+          max_life: true,
+          curr_san: true,
+          max_san: true,
+          curr_eff: true,
+          max_eff: true,
+          defesa_def: true,
+        },
+      },
+    },
+  });
+
+  return list;
+};
+
+module.exports = listCharactersForDashboardService;
