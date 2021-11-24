624698dda1a799b9cfbf35f9e7934b37f11a5ff4
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-24 08:11:36 -0300

    Create updateCharacterLifeService.js

---
+++ src/modules/character/updateCharacterLife/updateCharacterLifeService.js
@@ -0,0 +1,36 @@
+const prisma = require("../../../database");
+
+const updateCharacterLifeService = async ({
+  character_id,
+  currLife,
+  maxLife,
+}) => {
+  const sheet = await prisma.sheet.findFirst({
+    where: {
+      character_id,
+    },
+  });
+
+  if (!sheet) {
+    throw new Error("Couldn't find any character sheet.");
+  }
+
+  await prisma.sheet.updateMany({
+    where: {
+      character_id: sheet.character_id,
+    },
+    data: {
+      curr_life: currLife,
+      max_life: maxLife,
+    },
+  });
+
+  const responseObject = {
+    currLife,
+    maxLife,
+  };
+
+  return responseObject;
+};
+
+module.exports = updateCharacterLifeService;
