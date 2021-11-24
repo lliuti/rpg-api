41d74a7749c0e83285630ab2bf8dd2843fe97498
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-24 10:38:15 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,8 +2,16 @@ const dotenv = require("dotenv/config");
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +19,14 @@ app.use(routes);
 
 const PORT = process.env.PORT || 3333;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
+io.on("connection", (socket) => {
+  console.log(socket.id);
 });
+
+httpServer.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT} with websockets`);
+});
+
+// app.listen(PORT, () => {
+//   console.log(`Server (HTTP) running on port: ${PORT}`);
+// });
