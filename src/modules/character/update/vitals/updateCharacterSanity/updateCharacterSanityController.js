5274a3a3ade15c080deca7ec6d550d89413ffd83
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-25 03:25:06 -0300

    Create updateCharacterSanityController.js

---
+++ src/modules/character/update/vitals/updateCharacterSanity/updateCharacterSanityController.js
@@ -0,0 +1,19 @@
+const updateCharacterSanityService = require("./updateCharacterSanityService");
+
+const updateCharacterSanityController = async (req, res) => {
+  const { currSan, maxSan } = req.body;
+  const { character_id } = req.params;
+  try {
+    const sanityPoints = await updateCharacterSanityService({
+      character_id,
+      currSan,
+      maxSan,
+    });
+    return res.status(201).json(sanityPoints);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ error: err.message });
+  }
+};
+
+module.exports = updateCharacterSanityController;
