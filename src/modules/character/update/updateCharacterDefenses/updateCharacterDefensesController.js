3e91d106d2841b6ea578fe6c093beebf06db47cf
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-25 03:24:44 -0300

    Create updateCharacterDefensesController.js

---
+++ src/modules/character/update/updateCharacterDefenses/updateCharacterDefensesController.js
@@ -0,0 +1,15 @@
+const updateCharacterDefensesService = require("./updateCharacterDefensesService");
+
+const updateCharacterDefensesController = async (req, res) => {
+  const data = req.body;
+  const { character_id } = req.params;
+  try {
+    await updateCharacterDefensesService({ character_id, data });
+    return res.status(201).send();
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ error: err.message });
+  }
+};
+
+module.exports = updateCharacterDefensesController;
