a488974ea98a7c2c2e8fc7e19630824e99227f90
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-26 01:28:02 -0300

    Create sendMail.js

---
+++ src/utils/sendMail.js
@@ -0,0 +1,23 @@
+const nodemailer = require("nodemailer");
+
+const sendMail = async ({ email_to, email_subject, email_text }) => {
+  const transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_SERVICE,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: `"rpg platform 🎲" <${process.env.EMAIL_USER}>`,
+    to: email_to,
+    subject: email_subject,
+    text: email_text,
+    // html: "<b>Hello world?</b>",
+  });
+
+  console.log(`email sent: ${info.messageId}`);
+};
+
+module.exports = sendMail;
