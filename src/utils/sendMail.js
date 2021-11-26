1413e1fc8f2a5cf9f2b5dcb37c5db13f41a1a068
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-26 01:49:14 -0300

    Update sendMail.js

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -3,9 +3,15 @@ const nodemailer = require("nodemailer");
 const sendMail = async ({ email_to, email_subject, email_text }) => {
   const transporter = nodemailer.createTransport({
     service: process.env.EMAIL_SERVICE,
+    port: 465,
+    secure: true,
+    secureConnection: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
+    },
+    tls: {
+      rejectUnAuthorized: true,
     },
   });
 
