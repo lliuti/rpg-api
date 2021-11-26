d1d7e45f23386b00b3ad04bcf50d92653ac0fef7
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-26 01:25:49 -0300

    Create newPasswordEmailService.js

---
+++ src/modules/player/newPasswordEmail/newPasswordEmailService.js
@@ -0,0 +1,31 @@
+const prisma = require("../../../database/index");
+const sendMail = require("../../../utils/sendMail");
+
+const newPasswordEmailService = async (usernameOrEmail) => {
+  const player = await prisma.player.findFirst({
+    where: {
+      OR: [
+        {
+          username: usernameOrEmail,
+        },
+        {
+          email: usernameOrEmail,
+        },
+      ],
+    },
+  });
+
+  if (!player) {
+    throw new Error("Couldn't find any player with this username/email");
+  }
+
+  const email_to = player.email;
+  const email_subject = "Cadastrar uma nova senha - RPG";
+  const email_text = `Se voce, ${player.name}, clicou no botão "Esqueci minha senha", voce pode alterá-la para uma nova senha através dessa página: ${process.env.CLIENT_URL}/#/players/${player.id}/new-password`;
+
+  console.log(`${process.env.CLIENT_URL}/#/players/${player.id}/new-password`);
+
+  await sendMail({ email_to, email_subject, email_text });
+};
+
+module.exports = newPasswordEmailService;
