d603e3765609ba51a28ad2f0d6ba2b323d10ddd2
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-27 15:50:19 -0300

    Update deletePlayerController.js

--- src/modules/player/deletePlayer/deletePlayerController.js
+++ src/modules/player/deletePlayer/deletePlayerController.js
@@ -4,6 +4,7 @@ const deletePlayerController = async (req, res) => {
   const { player_id } = req.params;
   try {
     await deletePlayerService(player_id);
+    return res.status(204).send();
   } catch (err) {
     console.log(err);
     return res.status(400).json({ error: err.message });
