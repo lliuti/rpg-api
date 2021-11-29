4062be22e3d9946fb8cf6406c1d0e4e959461bb1
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-29 17:57:07 -0300

    Update assignRitualToCharacterController.js

--- src/modules/ritual/assignRitualToCharacter/assignRitualToCharacterController.js
+++ src/modules/ritual/assignRitualToCharacter/assignRitualToCharacterController.js
@@ -4,13 +4,11 @@ const assignRitualToCharacterController = async (req, res) => {
   const { character_id, ritual_id } = req.params;
 
   try {
-    const ritual = await assignRitualToCharacterService({
+    await assignRitualToCharacterService({
       character_id,
       ritual_id,
     });
-    return res.status(201).json({
-      success: `Ritual: ${ritual.ritual} assigned to: ${ritual.character}`,
-    });
+    return res.status(201).send();
   } catch (err) {
     console.log(err);
     return res.status(400).json({ error: err.message });
