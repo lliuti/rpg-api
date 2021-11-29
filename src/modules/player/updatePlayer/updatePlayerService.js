49c84181ff7e09ac5c85e9a88dcc827c9a07c980
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-29 00:27:45 -0300

    Update updatePlayerService.js

--- src/modules/player/updatePlayer/updatePlayerService.js
+++ src/modules/player/updatePlayer/updatePlayerService.js
@@ -1,12 +1,17 @@
 const prisma = require("../../../database");
 
 const updatePlayerService = async ({ player_id, data }) => {
-  await prisma.player.updateMany({
-    where: {
-      id: player_id,
-    },
-    data: data,
-  });
+  try {
+    await prisma.player.updateMany({
+      where: {
+        id: player_id,
+      },
+      data: data,
+    });
+  } catch (err) {
+    console.log(err);
+    throw new Error("Couldn't update player");
+  }
 };
 
 module.exports = updatePlayerService;
