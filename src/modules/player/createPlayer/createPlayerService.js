d654e76f9a3244b9213c733775eeaecdf68faeeb
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-29 00:27:32 -0300

    Update createPlayerService.js

--- src/modules/player/createPlayer/createPlayerService.js
+++ src/modules/player/createPlayer/createPlayerService.js
@@ -2,54 +2,59 @@ const prisma = require("../../../database/index");
 const bcryptjs = require("bcryptjs");
 
 const createPlayerService = async ({ name, username, email, password }) => {
-  const hashed_password = await bcryptjs.hash(password, 8);
-  const lowerUsername = username.toLowerCase();
-  const lowerEmail = email.toLowerCase();
+  try {
+    const hashed_password = await bcryptjs.hash(password, 8);
+    const lowerUsername = username.toLowerCase();
+    const lowerEmail = email.toLowerCase();
 
-  let player = await prisma.player.findFirst({
-    where: {
-      OR: [
-        {
-          email: lowerEmail,
-        },
-        {
-          username: lowerUsername,
-        },
-      ],
-    },
-    select: {
-      id: true,
-      name: true,
-      username: true,
-      email: true,
-      createdAt: true,
-    },
-  });
+    let player = await prisma.player.findFirst({
+      where: {
+        OR: [
+          {
+            email: lowerEmail,
+          },
+          {
+            username: lowerUsername,
+          },
+        ],
+      },
+      select: {
+        id: true,
+        name: true,
+        username: true,
+        email: true,
+        createdAt: true,
+      },
+    });
 
-  if (player) {
-    console.log(
-      `There is already a player with the username: ${lowerUsername} or email: ${lowerEmail}`
-    );
-    throw new Error("There is already a player with this email/username");
+    if (player) {
+      console.log(
+        `There is already a player with the username: ${lowerUsername} or email: ${lowerEmail}`
+      );
+      throw new Error("There is already a player with this email/username");
+    }
+
+    player = await prisma.player.create({
+      data: {
+        name,
+        username: lowerUsername,
+        email: lowerEmail,
+        password: hashed_password,
+      },
+      select: {
+        id: true,
+        name: true,
+        username: true,
+        email: true,
+        createdAt: true,
+      },
+    });
+
+    return player;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Couldn't create player");
   }
-
-  player = await prisma.player.create({
-    data: {
-      name,
-      username: lowerUsername,
-      email: lowerEmail,
-      password: hashed_password,
-    },
-    select: {
-      id: true,
-      name: true,
-      username: true,
-      email: true,
-      createdAt: true,
-    },
-  });
-
-  return player;
 };
 
 module.exports = createPlayerService;
