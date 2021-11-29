0c44e0eb0a29ee77ca5b430e2720519c09f358b0
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-29 00:26:57 -0300

    Update assignAttackToCharacterService.js

--- src/modules/attack/assignAttackToCharacter/assignAttackToCharacterService.js
+++ src/modules/attack/assignAttackToCharacter/assignAttackToCharacterService.js
@@ -1,34 +1,17 @@
 const prisma = require("../../../database");
 
 const assignAttackToCharacterService = async ({ character_id, attack_id }) => {
-  const attack = await prisma.attack.findFirst({
-    where: {
-      id: attack_id,
-    },
-  });
-
-  if (!attack) {
-    throw new Error("Couldn't find any attack");
+  try {
+    await prisma.characterAttacks.create({
+      data: {
+        attack_id: attack_id,
+        character_id: character_id,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    throw new Error("Couldn't assign attack to character");
   }
-
-  const character = await prisma.character.findFirst({
-    where: {
-      id: character_id,
-    },
-  });
-
-  if (!character) {
-    throw new Error("Couldn't find any character");
-  }
-
-  await prisma.characterAttacks.create({
-    data: {
-      attack_id: attack.id,
-      character_id: character.id,
-    },
-  });
-
-  return { attack: attack.name, character: character.name };
 };
 
 module.exports = assignAttackToCharacterService;
