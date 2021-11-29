dae1e1106a1ec4c05603e53448ff9239e5fab3c2
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-29 00:27:19 -0300

    Update updateCharacterPictureService.js

--- src/modules/character/update/updateCharacterPicture/updateCharacterPictureService.js
+++ src/modules/character/update/updateCharacterPicture/updateCharacterPictureService.js
@@ -1,14 +1,19 @@
 const prisma = require("../../../../database");
 
 const updateCharacterPictureService = async ({ character_id, picture_url }) => {
-  await prisma.character.updateMany({
-    where: {
-      id: character_id,
-    },
-    data: {
-      picture_url: picture_url,
-    },
-  });
+  try {
+    await prisma.character.updateMany({
+      where: {
+        id: character_id,
+      },
+      data: {
+        picture_url: picture_url,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    throw new Error("Couldn't update character picture");
+  }
 };
 
 module.exports = updateCharacterPictureService;
