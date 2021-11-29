2c73eb7c2b8dcc849dfcb48d6c40cecc3caad538
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-29 00:27:02 -0300

    Update searchAssignableAttacksService.js

--- src/modules/attack/searchAssignableAttacks/searchAssignableAttacksService.js
+++ src/modules/attack/searchAssignableAttacks/searchAssignableAttacksService.js
@@ -1,20 +1,25 @@
 const prisma = require("../../../database");
 
 const searchAssignableAttacksService = async (character_id) => {
-  const assignableAttacks = await prisma.attack.findMany({
-    where: {
-      characters: {
-        none: {
-          character_id,
+  try {
+    const assignableAttacks = await prisma.attack.findMany({
+      where: {
+        characters: {
+          none: {
+            character_id,
+          },
         },
       },
-    },
-    orderBy: {
-      name: "asc",
-    },
-  });
+      orderBy: {
+        name: "asc",
+      },
+    });
 
-  return assignableAttacks;
+    return assignableAttacks;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Couldn't search assignable attacks");
+  }
 };
 
 module.exports = searchAssignableAttacksService;
