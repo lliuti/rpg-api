ac5c8fdebce1d7626adb4aa9245c3cf4a11f64ea
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-29 00:27:56 -0300

    Update createRitualService.js

--- src/modules/ritual/createRitual/createRitualService.js
+++ src/modules/ritual/createRitual/createRitualService.js
@@ -11,21 +11,26 @@ const createRitualService = async ({
   area,
   duration,
 }) => {
-  const ritual = await prisma.ritual.create({
-    data: {
-      name,
-      description,
-      element,
-      circle,
-      cost,
-      range,
-      execTime,
-      area,
-      duration,
-    },
-  });
+  try {
+    const ritual = await prisma.ritual.create({
+      data: {
+        name,
+        description,
+        element,
+        circle,
+        cost,
+        range,
+        execTime,
+        area,
+        duration,
+      },
+    });
 
-  return ritual;
+    return ritual;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Couldn't create ritual");
+  }
 };
 
 module.exports = createRitualService;
