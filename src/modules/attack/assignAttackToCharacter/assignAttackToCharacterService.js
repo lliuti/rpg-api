edf15cf6f89dd416faa8631756dac4a20367725d
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-29 17:57:00 -0300

    Update assignAttackToCharacterService.js

--- src/modules/attack/assignAttackToCharacter/assignAttackToCharacterService.js
+++ src/modules/attack/assignAttackToCharacter/assignAttackToCharacterService.js
@@ -2,6 +2,18 @@ const prisma = require("../../../database");
 
 const assignAttackToCharacterService = async ({ character_id, attack_id }) => {
   try {
+    const alreadyAssigned = await prisma.characterAttacks.findFirst({
+      where: {
+        attack_id: attack_id,
+        character_id: character_id,
+      },
+    });
+
+    if (alreadyAssigned) {
+      console.log(err);
+      throw new Error("Attack already assigned!");
+    }
+
     await prisma.characterAttacks.create({
       data: {
         attack_id: attack_id,
