aeb46a5a276d9ae447ea4615bf893612ee0e3730
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-29 00:27:10 -0300

    Update listCharactersForDashboardService.js

--- src/modules/character/listCharactersForDashboard/listCharactersForDashboardService.js
+++ src/modules/character/listCharactersForDashboard/listCharactersForDashboardService.js
@@ -1,31 +1,36 @@
 const prisma = require("../../../database/index");
 
 const listCharactersForDashboardService = async () => {
-  const list = await prisma.character.findMany({
-    orderBy: {
-      createdAt: "asc",
-    },
-    select: {
-      id: true,
-      name: true,
-      archetype: true,
-      picture_url: true,
-      exp: true,
-      sheet: {
-        select: {
-          curr_life: true,
-          max_life: true,
-          curr_san: true,
-          max_san: true,
-          curr_eff: true,
-          max_eff: true,
-          defesa_def: true,
+  try {
+    const list = await prisma.character.findMany({
+      orderBy: {
+        createdAt: "asc",
+      },
+      select: {
+        id: true,
+        name: true,
+        archetype: true,
+        picture_url: true,
+        exp: true,
+        sheet: {
+          select: {
+            curr_life: true,
+            max_life: true,
+            curr_san: true,
+            max_san: true,
+            curr_eff: true,
+            max_eff: true,
+            defesa_def: true,
+          },
         },
       },
-    },
-  });
+    });
 
-  return list;
+    return list;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Couldn't list characters for dashboard");
+  }
 };
 
 module.exports = listCharactersForDashboardService;
