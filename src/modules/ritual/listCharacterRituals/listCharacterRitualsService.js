e9b87703e76e4d29e0ff407679d2483438a77e7b
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-29 00:27:57 -0300

    Update listCharacterRitualsService.js

--- src/modules/ritual/listCharacterRituals/listCharacterRitualsService.js
+++ src/modules/ritual/listCharacterRituals/listCharacterRitualsService.js
@@ -1,19 +1,24 @@
 const prisma = require("../../../database");
 
 const listCharacterRitualsService = async (character_id) => {
-  const rituals = await prisma.characterRituals.findMany({
-    where: {
-      character_id: character_id,
-    },
-    select: {
-      ritual: true,
-    },
-    orderBy: {
-      createdAt: "asc",
-    },
-  });
+  try {
+    const rituals = await prisma.characterRituals.findMany({
+      where: {
+        character_id: character_id,
+      },
+      select: {
+        ritual: true,
+      },
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
 
-  return rituals;
+    return rituals;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Couldn't list character rituals");
+  }
 };
 
 module.exports = listCharacterRitualsService;
