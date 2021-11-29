f1a2d68b080289a36130665bb1d5c2635726f95e
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-29 00:27:29 -0300

    Update rollDiceService.js

--- src/modules/dice/rollDice/rollDiceService.js
+++ src/modules/dice/rollDice/rollDiceService.js
@@ -6,45 +6,50 @@ const rollDiceService = async ({
   diceAmount,
   diceFaceAmount,
 }) => {
-  const character = await prisma.character.findFirst({
-    where: {
-      id: character_id,
-    },
-  });
+  try {
+    const character = await prisma.character.findFirst({
+      where: {
+        id: character_id,
+      },
+    });
 
-  const diceFaces = diceFaceAmount.replace("D", "");
-  const diceRolls = [];
-  let diceTotalResult = 0;
-  let diceGreater = 0;
+    const diceFaces = diceFaceAmount.replace("D", "");
+    const diceRolls = [];
+    let diceTotalResult = 0;
+    let diceGreater = 0;
 
-  for (i = 0; i < diceAmount; i++) {
-    const diceResult = Math.floor(Math.random() * diceFaces) + 1;
-    if (diceGreater < diceResult) {
-      diceGreater = diceResult;
+    for (i = 0; i < diceAmount; i++) {
+      const diceResult = Math.floor(Math.random() * diceFaces) + 1;
+      if (diceGreater < diceResult) {
+        diceGreater = diceResult;
+      }
+      diceTotalResult += diceResult;
+      diceRolls.push(diceResult);
     }
-    diceTotalResult += diceResult;
-    diceRolls.push(diceResult);
+
+    console.log(`\n${character.name} rolled ${diceAmount} d${diceFaces}`);
+    console.log(`[DICES] ${diceRolls} \n[SUM] ${diceTotalResult}\n`);
+
+    const responseObject = {
+      diceRolls: diceRolls,
+      diceResult: diceTotalResult,
+      diceGreater: diceGreater,
+    };
+
+    getSocketInstance.getSocketInstance().emit("diceRoll", {
+      character: character.name,
+      dices: diceAmount,
+      faces: diceFaces,
+      rolls: diceRolls,
+      totalResult: diceTotalResult,
+      greater: diceGreater,
+    });
+
+    return responseObject;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Couldn't roll dice");
   }
-
-  console.log(`\n${character.name} rolled ${diceAmount} d${diceFaces}`);
-  console.log(`[DICES] ${diceRolls} \n[SUM] ${diceTotalResult}\n`);
-
-  const responseObject = {
-    diceRolls: diceRolls,
-    diceResult: diceTotalResult,
-    diceGreater: diceGreater,
-  };
-
-  getSocketInstance.getSocketInstance().emit("diceRoll", {
-    character: character.name,
-    dices: diceAmount,
-    faces: diceFaces,
-    rolls: diceRolls,
-    totalResult: diceTotalResult,
-    greater: diceGreater,
-  });
-
-  return responseObject;
 };
 
 module.exports = rollDiceService;
