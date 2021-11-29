ffdd75536590c95a6de379187e1d07071ebbe9e7
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-29 17:57:10 -0300

    Update assignRitualToCharacterService.js

--- src/modules/ritual/assignRitualToCharacter/assignRitualToCharacterService.js
+++ src/modules/ritual/assignRitualToCharacter/assignRitualToCharacterService.js
@@ -2,6 +2,18 @@ const prisma = require("../../../database");
 
 const assignRitualToCharacterService = async ({ character_id, ritual_id }) => {
   try {
+    const alreadyAssigned = await prisma.characterRituals.findFirst({
+      where: {
+        ritual_id: ritual_id,
+        character_id: character_id,
+      },
+    });
+
+    if (alreadyAssigned) {
+      console.log(err);
+      throw new Error("Ritual already assigned!");
+    }
+
     await prisma.characterRituals.create({
       data: {
         ritual_id: ritual_id,
