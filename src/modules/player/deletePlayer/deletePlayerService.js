aa7aba075208efe15d36531be2d6afe7d6a7ea69
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-29 00:27:35 -0300

    Update deletePlayerService.js

--- src/modules/player/deletePlayer/deletePlayerService.js
+++ src/modules/player/deletePlayer/deletePlayerService.js
@@ -1,11 +1,16 @@
 const prisma = require("../../../database/index");
 
 const deletePlayerService = async (player_id) => {
-  await prisma.player.deleteMany({
-    where: {
-      id: player_id,
-    },
-  });
+  try {
+    await prisma.player.deleteMany({
+      where: {
+        id: player_id,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    throw new Error("Couldn't delete player");
+  }
 };
 
 module.exports = deletePlayerService;
