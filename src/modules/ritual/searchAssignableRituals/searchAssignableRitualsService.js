6202260ed5bd8d276172915c7a4571565b000af0
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-29 00:28:01 -0300

    Update searchAssignableRitualsService.js

--- src/modules/ritual/searchAssignableRituals/searchAssignableRitualsService.js
+++ src/modules/ritual/searchAssignableRituals/searchAssignableRitualsService.js
@@ -1,20 +1,25 @@
 const prisma = require("../../../database");
 
 const searchAssignableRitualsService = async (character_id) => {
-  const assignableRituals = await prisma.ritual.findMany({
-    where: {
-      characters: {
-        none: {
-          character_id,
+  try {
+    const assignableRituals = await prisma.ritual.findMany({
+      where: {
+        characters: {
+          none: {
+            character_id,
+          },
         },
       },
-    },
-    orderBy: {
-      name: "asc",
-    },
-  });
+      orderBy: {
+        name: "asc",
+      },
+    });
 
-  return assignableRituals;
+    return assignableRituals;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Couldn't search assignable rituals");
+  }
 };
 
 module.exports = searchAssignableRitualsService;
