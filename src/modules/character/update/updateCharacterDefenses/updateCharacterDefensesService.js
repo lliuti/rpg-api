0b21d8985845ca611505172bdf41ee19284c1d68
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-29 00:27:15 -0300

    Update updateCharacterDefensesService.js

--- src/modules/character/update/updateCharacterDefenses/updateCharacterDefensesService.js
+++ src/modules/character/update/updateCharacterDefenses/updateCharacterDefensesService.js
@@ -1,28 +1,17 @@
 const prisma = require("../../../../database");
 
 const updateCharacterDefensesService = async ({ character_id, data }) => {
-  const characterSheet = await prisma.sheet.findFirst({
-    where: {
-      character_id,
-    },
-  });
-
-  if (!characterSheet) {
-    throw new Error("Couldn't find any character sheet");
+  try {
+    await prisma.sheet.updateMany({
+      where: {
+        character_id: character_id,
+      },
+      data: data,
+    });
+  } catch (err) {
+    console.log(err);
+    throw new Error("Couldn't update character defenses");
   }
-
-  await prisma.sheet.updateMany({
-    where: {
-      character_id: character_id,
-    },
-    data: data,
-  });
-
-  console.log(
-    `${
-      characterSheet.character_id
-    } updated character defenses to: ${JSON.stringify(data)}`
-  );
 };
 
 module.exports = updateCharacterDefensesService;
