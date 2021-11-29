4d4bfcf7309edf2d58753f6c19246def63cb9ed1
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-29 00:26:55 -0300

    Update assignAttackToCharacterController.js

--- src/modules/attack/assignAttackToCharacter/assignAttackToCharacterController.js
+++ src/modules/attack/assignAttackToCharacter/assignAttackToCharacterController.js
@@ -4,15 +4,11 @@ const assignAttackToCharacterController = async (req, res) => {
   const { character_id, attack_id } = req.params;
 
   try {
-    const attack = await assignAttackToCharacterService({
+    await assignAttackToCharacterService({
       character_id,
       attack_id,
     });
-    return res
-      .status(201)
-      .json({
-        success: `Attack: ${attack.attack} assigned to: ${attack.character}`,
-      });
+    return res.status(201).send();
   } catch (err) {
     console.log(err);
     return res.status(400).json({ error: err.message });
