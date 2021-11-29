4069abe6d8c97264f5b05518d3d20006b70ec85e
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-29 00:27:26 -0300

    Update updateCharacterSanityService.js

--- src/modules/character/update/vitals/updateCharacterSanity/updateCharacterSanityService.js
+++ src/modules/character/update/vitals/updateCharacterSanity/updateCharacterSanityService.js
@@ -6,54 +6,59 @@ const updateCharacterSanityService = async ({
   currSan,
   maxSan,
 }) => {
-  const sheet = await prisma.sheet.findFirst({
-    where: {
-      character_id,
-    },
-  });
+  try {
+    const sheet = await prisma.sheet.findFirst({
+      where: {
+        character_id,
+      },
+    });
 
-  if (!sheet) {
-    throw new Error("Couldn't find any character sheet.");
+    if (!sheet) {
+      throw new Error("Couldn't find any character sheet.");
+    }
+
+    const character = await prisma.character.findFirst({
+      where: {
+        id: sheet.character_id,
+      },
+    });
+
+    const previousCurrVital = sheet.curr_san;
+    const previousMaxVital = sheet.max_san;
+    const currVital = currSan;
+    const maxVital = maxSan;
+
+    await prisma.sheet.updateMany({
+      where: {
+        character_id: sheet.character_id,
+      },
+      data: {
+        curr_san: currSan,
+        max_san: maxSan,
+      },
+    });
+
+    const responseObject = {
+      currVital,
+      maxVital,
+      previousCurrVital,
+      previousMaxVital,
+    };
+
+    getSocketInstance.getSocketInstance().emit("vitalsChanged", {
+      character: character.name,
+      vital: "sanity",
+      currVital,
+      maxVital,
+      previousCurrVital,
+      previousMaxVital,
+    });
+
+    return responseObject;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Couldn't update character sanity");
   }
-
-  const character = await prisma.character.findFirst({
-    where: {
-      id: sheet.character_id,
-    },
-  });
-
-  const previousCurrVital = sheet.curr_san;
-  const previousMaxVital = sheet.max_san;
-  const currVital = currSan;
-  const maxVital = maxSan;
-
-  await prisma.sheet.updateMany({
-    where: {
-      character_id: sheet.character_id,
-    },
-    data: {
-      curr_san: currSan,
-      max_san: maxSan,
-    },
-  });
-
-  const responseObject = {
-    currVital,
-    maxVital,
-    previousCurrVital,
-    previousMaxVital,
-  };
-
-  getSocketInstance.getSocketInstance().emit("vitalsChanged", {
-    character: character.name,
-    vital: "sanity",
-    currVital,
-    maxVital,
-    previousCurrVital,
-    previousMaxVital,
-  });
-
-  return responseObject;
 };
 
 module.exports = updateCharacterSanityService;
