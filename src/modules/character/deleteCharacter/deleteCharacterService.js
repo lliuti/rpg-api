fd69b990694542c161c2d93f5a929b6441ce6cc4
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-29 00:27:06 -0300

    Update deleteCharacterService.js

--- src/modules/character/deleteCharacter/deleteCharacterService.js
+++ src/modules/character/deleteCharacter/deleteCharacterService.js
@@ -1,11 +1,16 @@
 const prisma = require("../../../database/index");
 
 const deleteCharacterService = async (character_id) => {
-  await prisma.character.deleteMany({
-    where: {
-      id: character_id,
-    },
-  });
+  try {
+    await prisma.character.deleteMany({
+      where: {
+        id: character_id,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    throw new Error("Couldn't delete character");
+  }
 };
 
 module.exports = deleteCharacterService;
