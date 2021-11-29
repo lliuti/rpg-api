a137aa737aa83f227ef88141f72db93ee882f7ee
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-29 00:27:44 -0300

    Update updatePasswordService.js

--- src/modules/player/updatePassword/updatePasswordService.js
+++ src/modules/player/updatePassword/updatePasswordService.js
@@ -2,26 +2,31 @@ const bcryptjs = require("bcryptjs");
 const prisma = require("../../../database/index");
 
 const updatePasswordService = async ({ newPassword, player_id }) => {
-  const newHashedPassword = await bcryptjs.hash(newPassword, 8);
+  try {
+    const newHashedPassword = await bcryptjs.hash(newPassword, 8);
 
-  const player = await prisma.player.findFirst({
-    where: {
-      id: player_id,
-    },
-  });
+    const player = await prisma.player.findFirst({
+      where: {
+        id: player_id,
+      },
+    });
 
-  if (!player) {
-    throw new Error("Couldn't find any player");
+    if (!player) {
+      throw new Error("Couldn't find any player");
+    }
+
+    await prisma.player.updateMany({
+      where: {
+        id: player.id,
+      },
+      data: {
+        password: newHashedPassword,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    throw new Error("Couldn't update password");
   }
-
-  await prisma.player.updateMany({
-    where: {
-      id: player.id,
-    },
-    data: {
-      password: newHashedPassword,
-    },
-  });
 };
 
 module.exports = updatePasswordService;
