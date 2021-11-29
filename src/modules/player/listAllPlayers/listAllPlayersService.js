ab53e2e105c20ac7ccdae2f8d83e78e159c9c373
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-29 00:27:39 -0300

    Update listAllPlayersService.js

--- src/modules/player/listAllPlayers/listAllPlayersService.js
+++ src/modules/player/listAllPlayers/listAllPlayersService.js
@@ -1,22 +1,27 @@
 const prisma = require("../../../database");
 
 const listAllPlayersService = async () => {
-  const list = await prisma.player.findMany({
-    select: {
-      id: true,
-      name: true,
-      username: true,
-      email: true,
-      password: false,
-      admin: true,
-      createdAt: true,
-      updatedAt: true,
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
-  return list;
+  try {
+    const list = await prisma.player.findMany({
+      select: {
+        id: true,
+        name: true,
+        username: true,
+        email: true,
+        password: false,
+        admin: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return list;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Couldn't list all players");
+  }
 };
 
 module.exports = listAllPlayersService;
