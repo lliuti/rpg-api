a6829cd356310d5c11956e2f1c08dec2f5cd06f6
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-30 21:05:24 -0300

    Update getCharacterSheetController.js

--- src/modules/character/getCharacterSheet/getCharacterSheetController.js
+++ src/modules/character/getCharacterSheet/getCharacterSheetController.js
@@ -2,9 +2,13 @@ const getCharacterSheetService = require("./getCharacterSheetService");
 
 const getCharacterSheetController = async (req, res) => {
   const { character_id } = req.params;
+  const { player_id } = req;
 
   try {
-    const characterSheet = await getCharacterSheetService(character_id);
+    const characterSheet = await getCharacterSheetService({
+      character_id,
+      player_id,
+    });
     return res.status(200).json(characterSheet);
   } catch (err) {
     console.log(err);
