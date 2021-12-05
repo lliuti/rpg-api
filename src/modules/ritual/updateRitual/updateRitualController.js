2205d7f3288f68b24985db86b5ebbc2936eba161
Lucas Liuti <dev.lliuti@gmail.com>
2021-12-05 01:50:11 -0300

    Update updateRitualController.js

--- src/modules/ritual/updateRitual/updateRitualController.js
+++ src/modules/ritual/updateRitual/updateRitualController.js
@@ -2,7 +2,7 @@ const updateRitualService = require("./updateRitualService");
 
 const updateRitualController = async (req, res) => {
   const data = req.body;
-  const { attack_id } = req.params;
+  const { ritual_id } = req.params;
 
   try {
     await updateRitualService({ ritual_id, data });
