e0e91f5d08995992d377bb50008300acf47c7ab3
Lucas Liuti <dev.lliuti@gmail.com>
2021-12-27 23:21:30 -0300

    Create updateRitualsService.js

---
+++ src/modules/ritual/updateRituals/updateRitualsService.js
@@ -0,0 +1,14 @@
+const prisma = require("../../../database");
+
+const updateRitualsService = async (data) => {
+  try {
+    await prisma.ritual.updateMany({
+      data: data,
+    });
+  } catch (err) {
+    console.log(err);
+    throw new Error("Couldn't edit ritual");
+  }
+};
+
+module.exports = updateRitualsService;
