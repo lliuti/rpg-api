fa0839cf50ea2d7be146b398e0e0769e56ce5241
Lucas Liuti <dev.lliuti@gmail.com>
2021-12-27 23:21:31 -0300

    Create updateRitualsController.js

---
+++ src/modules/ritual/updateRituals/updateRitualsController.js
@@ -0,0 +1,15 @@
+const updateRitualsService = require("./updateRitualsService");
+
+const updateRitualsController = async (req, res) => {
+  const data = req.body;
+
+  try {
+    await updateRitualsService(data);
+    return res.status(201).send();
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ error: err.message });
+  }
+};
+
+module.exports = updateRitualsController;
