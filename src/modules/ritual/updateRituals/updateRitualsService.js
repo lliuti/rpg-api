08e3e3233f50d9b67310adb89ee5e98c2bfe6bf0
Lucas Liuti <dev.lliuti@gmail.com>
2021-12-29 14:12:08 -0300

    Update updateRitualsService.js

--- src/modules/ritual/updateRituals/updateRitualsService.js
+++ src/modules/ritual/updateRituals/updateRitualsService.js
@@ -3,7 +3,7 @@ const prisma = require("../../../database");
 const updateRitualsService = async (data) => {
   try {
     await prisma.ritual.updateMany({
-      data: data,
+      data,
     });
   } catch (err) {
     console.log(err);
