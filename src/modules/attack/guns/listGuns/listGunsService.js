9146082f81c330b4982af6303004e01d14d10b18
Lucas Liuti <dev.lliuti@gmail.com>
2022-01-04 10:57:11 -0300

    Create listGunsService.js

---
+++ src/modules/attack/guns/listGuns/listGunsService.js
@@ -0,0 +1,30 @@
+const prisma = require("../../../../database");
+
+const listGunsService = async (character_id) => {
+  try {
+    const list = await prisma.characterAttacks.findMany({
+      where: {
+        attack: {
+          type: "Arma de Fogo",
+        },
+        character_id,
+      },
+      select: {
+        attack: {
+          select: {
+            name: true,
+            ammo: true,
+            id: true,
+          },
+        },
+        curr_shots: true,
+      },
+    });
+
+    return list;
+  } catch (err) {
+    throw new Error("Couldn't find character guns");
+  }
+};
+
+module.exports = listGunsService;
